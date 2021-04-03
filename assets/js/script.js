e128c1c7971ad4e7fcc3bb2581b7696ad506494f
jasonmueller <jmuel9999@gmail.com>
2021-04-03 14:19:27 -0500

    added displayTimeBlocks function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 
 //current date and time
-var timeDisplay = function() {
+const timeDisplay = function() {
     var todaysDate = moment()._d;
     var showTime = document.querySelector("#currentDay");
 
@@ -12,6 +12,33 @@ var timeDisplay = function() {
 //when user scrolls, show time blocks
 
 //each time block color coded based on past, present or future
-var =
+const displayTimeBlocks = function () {
+    var currentTime = moment()._d;
+    console.log(moment(currentTime).hour());//grabs current time hour
+    // console.log(moment(currentTime).isBefore("2021-04-02T23:00"));
+    const textAreas = document.querySelectorAll(".textarea");//added dot due to class
+    console.log(textAreas[0].id);
+    //substring method
+    console.log(textAreas[0].id.substring(9, 11));
+    const textAreaHour = textAreas[0].id.substring(9, 11);
+    const currentTimeHour = moment(currentTime).hour();
+    console.log(textAreaHour > currentTimeHour);
+    for (let i = 0; i < textAreas.length; i++) {
+        console.log(textAreas[i].id);
+        const hour = textAreas[i].id.substring(9, 11);//practice this
+        if (hour > currentTimeHour) {
+            console.log(hour + " is after the current time.");
+        } else if (hour < currentTimeHour) {
+            const el = document.getElementById("textarea-" + hour);
+            console.log(hour + " is before the current time.");
+            console.log("mark");
+            el.classList.add("past");
+        } else {
+            console.log(hour + " is the current hour.");
+        }
+    }
+
+};
 
 timeDisplay();
+displayTimeBlocks();
